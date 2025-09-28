ea85547e2efa4d47d68eb88b0693e7da69934a80
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2025-09-28 10:40:14 +0700

    update: update init script and sample query script

--- database/select-query.sql
+++ database/select-query.sql
@@ -1,18 +1,30 @@
 USE dbop;
 SET NAMES utf8mb4;
 
+-- Alter tables
+ALTER TABLE Roles
+ADD COLUMN deleted BOOLEAN DEFAULT 0;
+
+ALTER TABLE Users
+ADD COLUMN deleted BOOLEAN DEFAULT 0;
+
+ALTER TABLE Quotes
+ADD COLUMN deleted BOOLEAN DEFAULT 0;
+
 -- Select users
 SELECT COUNT(id) FROM Users;
 
-SELECT * FROM Users;
+SELECT * FROM Users WHERE deleted IS FALSE;
 
 EXPLAIN SELECT * FROM Users;
 
 SELECT roleId, username, email, fullName, phone, birthDate, bio
-FROM Users;
+FROM Users
+WHERE deleted IS FALSE;
 
 EXPLAIN SELECT roleId, username, email, fullName, phone, birthDate, bio
-FROM Users;
+FROM Users
+WHERE deleted IS FALSE;
 
 -- Select quotes
 SELECT COUNT(id) FROM Quotes;
@@ -21,18 +33,18 @@ SELECT COUNT(userId), userId
 FROM Quotes
 GROUP BY userId;
 
-SELECT * FROM Quotes;
+SELECT * FROM Quotes WHERE deleted IS FALSE;
 
 SELECT id, userId, title, description
 FROM Quotes
-WHERE id = 23425;
+WHERE id = 23425 AND deleted IS FALSE;
 
 SELECT q.id, q.userId, q.title, q.description, u.username, u.fullName
 FROM Quotes as q
 	LEFT JOIN Users as u ON u.id = q.userId
-WHERE u.id = 3;
+WHERE u.id = 3 AND q.deleted IS FALSE;
 
 SELECT q.id, q.userId, q.title, q.description, u.username, u.fullName
 FROM Quotes as q
 	LEFT JOIN Users as u ON u.id = q.userId
-WHERE q.id = 42832;
+WHERE q.id = 42832 AND q.deleted IS FALSE;
